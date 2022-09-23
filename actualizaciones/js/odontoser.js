bd92da061a9884a6cf2c22b855bdbd0fb7ba4b63
edansama96 <104570856+edansama96@users.noreply.github.com>
2022-09-23 17:53:13 -0500

    Actualizaciones de las páginas
    Se realizaron la actualización como modificación de las páginas, además de la adición de la imagen svg para poder modificar y trabajar con ella

---
+++ actualizaciones/js/odontoser.js
@@ -0,0 +1,16 @@
+function mostrarAlerta(elemento){
+      
+    document.getElementById(elemento).setAttribute('fill','blue');
+    console.log(elemento);
+     return elemento;
+}
+
+function cambioColor() {
+    document.eleme
+    if(document.getElementById('check1').checked ){
+       document.getElementById("_x33_8" ).setAttribute('fill','green');
+       
+       
+    }
+    
+ }
